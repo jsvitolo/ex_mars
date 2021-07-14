56f611827f59277863af3cf4aa4223923b28ebad
Jones Vitolo <jsvitolo@gmail.com>
2021-07-14 10:18:35 -0300

    Add run commands

--- lib/ex_mars.ex
+++ lib/ex_mars.ex
@@ -1,18 +1,65 @@
 defmodule ExMars do
   @moduledoc """
-  Documentation for `ExMars`.
+  ExMars
   """
 
-  @doc """
-  Hello world.
+  alias ExMars.{Rover, Rover.Position, WorldMap}
 
-  ## Examples
+  def start(inputs) do
+    case ExMars.InputInterpreter.interpret(inputs) do
+      {:error, reason} ->
+        reason
 
-      iex> ExMars.hello()
-      :world
+      {world_map_size, rovers} ->
+        {:ok, world_map} = WorldMap.start_link(size: world_map_size)
+        build_rovers(world_map, rovers)
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defp build_rovers(world_map, rovers) do
+    rovers
+    |> Enum.map(fn rover ->
+      Task.async(fn -> build_rover(world_map, rover) end)
+    end)
+    |> Enum.map(fn pid ->
+      Task.await(pid)
+    end)
+  end
+
+  defp build_rover(world_map, %Rover{position: position} = rover) do
+    case WorldMap.deploy_rover(world_map, position.x, position.y, position) do
+      :ok ->
+        execute_commands(world_map, rover)
+
+      {:error, reason} ->
+        %Rover{rover | error: reason}
+    end
+  end
+
+  defp execute_commands(_world_map, %Rover{commands: []} = rover) do
+    rover
+  end
+
+  defp execute_commands(
+         world_map,
+         %Rover{commands: [command | commands], position: position} = rover
+       ) do
+    new_position = Position.apply(position, command)
+
+    WorldMap.update_rover(
+      world_map,
+      position.x,
+      position.y,
+      new_position.x,
+      new_position.y,
+      new_position
+    )
+    |> case do
+      :ok ->
+        execute_commands(world_map, %Rover{rover | commands: commands, position: new_position})
+
+      {:error, reason} ->
+        %Rover{rover | error: reason}
+    end
   end
 end
--- lib/ex_mars/cli.ex
+++ lib/ex_mars/cli.ex
@@ -1,10 +1,8 @@
 defmodule ExMars.CLI do
   def main([file]) do
     case File.read(file) do
-      {:ok, commands} ->
-        ExMars.InputInterpreter.interpret(commands)
-
-      # |> suppress_output(opts)
+      {:ok, inputs} ->
+        ExMars.start(inputs) |> visualize()
 
       {:error, _} ->
         {:error, "couldn't read #{file}"}
@@ -13,5 +11,17 @@ defmodule ExMars.CLI do
 
   def main(_) do
     IO.puts("Usage: toy_robot commands.txt")
+  end
+
+  defp visualize({:error, message}) do
+    IO.puts(:stderr, "Error: #{message}")
+  end
+
+  defp visualize(string) when is_binary(string) do
+    IO.puts(string)
+  end
+
+  defp visualize(list) when is_list(list) do
+    IO.puts(Enum.join(list, "\n"))
   end
 end
