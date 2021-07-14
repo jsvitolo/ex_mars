09ce124361191200e7a676335842b0f700c15c43
Jones Vitolo <jsvitolo@gmail.com>
2021-07-14 10:34:49 -0300

    Add escript

--- lib/ex_mars/cli.ex
+++ lib/ex_mars/cli.ex
@@ -1,16 +1,28 @@
 defmodule ExMars.CLI do
   def main([file]) do
-    case File.read(file) do
-      {:ok, inputs} ->
-        ExMars.start(inputs) |> visualize()
-
-      {:error, _} ->
-        {:error, "couldn't read #{file}"}
+    with :ok <- file_exists?(file),
+         {:ok, inputs} <- read_file(file) do
+      ExMars.start(inputs) |> visualize()
     end
   end
 
   def main(_) do
-    IO.puts("Usage: toy_robot commands.txt")
+    IO.puts("Usage: ./ex_mars test/support.input.txt")
+  end
+
+  defp file_exists?(file) do
+    if File.exists?(file) do
+      :ok
+    else
+      IO.puts("The file #{file} does not exist")
+    end
+  end
+
+  defp read_file(file) do
+    case File.read(file) do
+      {:ok, inputs} -> {:ok, inputs}
+      {:error, _} -> {:error, "couldn't read #{file}"}
+    end
   end
 
   defp visualize({:error, message}) do
