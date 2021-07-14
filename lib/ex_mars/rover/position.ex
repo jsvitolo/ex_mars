caf204fe50004277e88d0f0206689deb58c256c2
Jones Vitolo <jsvitolo@gmail.com>
2021-07-14 10:14:26 -0300

    Improving code

--- lib/ex_mars/rover/position.ex
+++ lib/ex_mars/rover/position.ex
@@ -1,7 +1,7 @@
 defmodule ExMars.Rover.Position do
   defstruct x: nil, y: nil, direction: nil
 
-  def apply_command(%__MODULE__{} = position, "R") do
+  def apply(%__MODULE__{} = position, "R") do
     case position.direction do
       "N" -> %__MODULE__{position | direction: "E"}
       "S" -> %__MODULE__{position | direction: "W"}
@@ -9,7 +9,8 @@ defmodule ExMars.Rover.Position do
       "W" -> %__MODULE__{position | direction: "N"}
     end
   end
-  def apply_command(%__MODULE__{} = position, "L") do
+
+  def apply(%__MODULE__{} = position, "L") do
     case position.direction do
       "N" -> %__MODULE__{position | direction: "W"}
       "S" -> %__MODULE__{position | direction: "E"}
@@ -17,12 +18,19 @@ defmodule ExMars.Rover.Position do
       "W" -> %__MODULE__{position | direction: "S"}
     end
   end
-  def apply_command(%__MODULE__{} = position, "M") do
+
+  def apply(%__MODULE__{} = position, "M") do
     case position.direction do
       "N" -> %__MODULE__{position | y: position.y + 1}
       "S" -> %__MODULE__{position | y: position.y - 1}
       "E" -> %__MODULE__{position | x: position.x + 1}
       "W" -> %__MODULE__{position | x: position.x - 1}
+    end
+  end
+
+  defimpl String.Chars, for: __MODULE__ do
+    def to_string(position) do
+      "#{position.x} #{position.y} #{position.direction}"
     end
   end
 end
